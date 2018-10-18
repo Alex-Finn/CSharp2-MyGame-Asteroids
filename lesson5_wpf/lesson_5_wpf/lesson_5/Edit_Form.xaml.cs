f9002cb3e27478af1dc2ced95f42ffee96536958
Alexey Elshin <finntroll.alex@gmail.com>
2018-10-18 16:56:46 +0300

    Добавлена форма редактирования employee (без функционала)

---
+++ lesson5_wpf/lesson_5_wpf/lesson_5/Edit_Form.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lesson_5
+{
+    /// <summary>
+    /// Логика взаимодействия для Edit_Form.xaml
+    /// </summary>
+    public partial class Edit_Form : Window
+    {
+        public Edit_Form(Employee employee)
+        {
+            InitializeComponent();
+            //cb_edit_department.ItemsSource = employee;
+            FillData(employee);
+        }
+        
+
+        private static void FillData(Employee employee)
+        {
+            
+             
+            //ComboBox.bin
+        }
+    }
+}
