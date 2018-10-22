9f60d6e55138c589fdbf84c129e1e7251009b659
finntroll_alex <frodo8787@mail.ru>
2018-10-22 20:46:18 +0300

    Вернул "потерявшийся" файл Department.cs

---
+++ lesson_5/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_5
+{
+    public class Department
+    {
+        public int Dept_Id { get; set; }
+        public Department(int _id)
+        {
+            Dept_Id = _id;
+        }
+        /// <summary>
+        /// default cotr
+        /// </summary>
+        public Department()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Department {Dept_Id}";
+        }
+    }
+}
